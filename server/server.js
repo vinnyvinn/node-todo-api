b7e53c7841b394cbcabd9b56eafdea5864ee1112
vinnyvinn <kituyiv@gmail.com>
2019-02-08 15:34:04 +0300

    add Get by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -29,6 +30,25 @@ Todo.find().then((todos)=>{
 	res.status(400).send(err);
 })
 });
+
+app.get('/todos/:id',(req,res)=>{
+	var id = req.params.id;
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+		if(!todo){
+			return res.status(404).send();
+		}
+		res.send({todo});
+
+	}).catch((e) =>{
+		res.status(400).send();
+	})
+});
+
 app.listen(3000,()=>{
 console.log('Started listening on port 3000');
 })
