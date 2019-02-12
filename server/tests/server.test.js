b3f23acde0466132d889da8252d10f765147ca95
vinnyvinn <kituyiv@gmail.com>
2019-02-12 07:06:02 +0300

    Setup separate test and development envs

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -193,8 +193,8 @@ var hexId = todos[1]._id.toHexString();
     .expect(200)
     .expect((res) =>{
     	expect(res.body.todo.text).toBe(text);
-    	expect(res.body.todo.completed).toBe(true);
-    	expect(res.body.todo.completedAt).toBeA('number');
+    	expect(res.body.todo.completed).toBe(false);
+    	expect(res.body.todo.completedAt).toNotExist();
     })
     .end(done);
 	})
